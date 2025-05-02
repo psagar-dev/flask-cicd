28c51beae1bd0781e65ec30f008b9cf5d46231c8
Sagar Patel <patelsagar.developer@gmail.com>
2025-05-02 20:00:20 +0530

    update

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from app import app
+
+def test_home_route():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Hello, Flask CI/CD!" in response.data
