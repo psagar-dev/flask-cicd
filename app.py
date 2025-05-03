37ea2a08fde690d9319616e6f2938efc49a30b03
Sagar Patel <patelsagar.developer@gmail.com>
2025-05-03 17:48:55 +0530

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Flask CI/CD Implement!"
+    return "Hello, Flask CI/CD Sagar!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
